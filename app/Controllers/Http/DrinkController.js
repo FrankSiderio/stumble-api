97c1f37a008fd7cbd6a1fa5d7c773869d8f2e1d7
FrankSiderio <frank_siderio@comcast.com>
2020-05-21 16:34:17 -0500

    remove a drink

--- app/Controllers/Http/DrinkController.js
+++ app/Controllers/Http/DrinkController.js
@@ -5,8 +5,6 @@ const Match = use('App/Models/Match')
 
 class DrinkController {
   async store({ request }) {
-    console.log(request.input('match'))
-    console.log(request.input('player'))
     const match = await Match.findByOrFail('identifier', request.input('match'))
 
     const drink = await Drink.findOrCreate({
@@ -19,6 +17,22 @@ class DrinkController {
     })
 
     drink.amount++
+    await drink.save()
+
+    return {
+      drink
+    }
+  }
+
+  async destroy({ request }) {
+    const match = await Match.findByOrFail('identifier', request.input('match'))
+
+    const drink = await Drink.findByOrFail({
+      player_id: request.input('player'),
+      match_id: match.id
+    })
+
+    drink.amount--
     await drink.save()
 
     return {
